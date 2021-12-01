461eb97d1fb9f14ee67562140bedefa6866a894f
Bernardo Sandoval <bernyy0427@gmail.com>
2021-12-01 13:50:14 -0600

    fucionalidad para validar que los datos ingresados sean numeros ok

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ module.exports = {
      * @param {*} num2 Numero 2 de la suma
      */
     suma: function (num1, num2){
-        return `EL resultado de sumar ${num1} + ${num2} es ${num1 + num2}`
+        return (this.esNumero(num1, num2) ? `EL resultado de sumar ${num1} + ${num2} es ${num1 + num2}` : this.mensajeError()) 
     },
     /**
      * Resta de dos numeros
@@ -17,7 +17,7 @@ module.exports = {
      * @param {*} num2 Numero 2 de la resta
      */
     resta: function (num1, num2){
-        return `EL resultado de restar ${num1} - ${num2} es ${num1 - num2}`
+        return (this.esNumero(num1, num2) ? `EL resultado de restar ${num1} - ${num2} es ${num1 - num2}` : this.mensajeError()) 
     },
     /**
      * Multiplicacion de dos numeros
@@ -27,7 +27,7 @@ module.exports = {
      * @param {*} num2 Numero 2 de la multiplicacion
      */
     multiplicacion: function (num1, num2){
-        return`EL resultado de multiplicar ${num1} * ${num2} es ${num1 * num2}`
+        return(this.esNumero(num1, num2) ? `EL resultado de multiplicar ${num1} * ${num2} es ${num1 * num2}` : this.mensajeError() ) 
     },
     /**
      * Division de dos numeros
@@ -37,6 +37,24 @@ module.exports = {
      * @param {*} num2 Numero 2 de la division
      */
     division: function (num1, num2){
-        return `EL resultado de dividir ${num1} / ${num2} es ${num1 / num2}`
+        return (this.esNumero(num1, num2) ? `EL resultado de dividir ${num1} / ${num2} es ${num1 / num2}` : this.mensajeError()) 
+    },
+    /**
+     * Mensaje de error que se ejecuta cuando no tengamos valores numericos
+     */
+    mensajeError: function() {
+        console.log('Uno o los dos valores que se han introducido no son numeros')
+    },
+    /**
+     * Comprueba que los valores ingresados sean numericos
+     * @param {*} num1 
+     * @param {*} num2 
+     * @returns 
+     */
+    esNumero: function (num1, num2) {
+        if(typeof num1 !== 'number' || typeof num2 !== 'number'){
+            return false
+        }
+        return true
     }
 }
--- pruebas.js
+++ pruebas.js
@@ -1,6 +1,20 @@
 const mod = require('.')
 
-console.log(mod.division(15, 3))
+
 console.log(mod.suma(2, 8))
-console.log(mod.multiplicacion(3, 7))
+console.log(mod.suma('a', 8))
+console.log(mod.suma(2, 'b'))
+console.log(mod.suma(true, 'b'))
+
 console.log(mod.resta(80, 50))
+console.log(mod.resta('a', 50))
+console.log(mod.resta('a', false))
+
+console.log(mod.multiplicacion(3, 7))
+console.log(mod.multiplicacion('a', false))
+
+console.log(mod.division(15, 3))
+console.log(mod.division('c', 2))
+
+
+
